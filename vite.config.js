134736f9bd16e4dd6cace299c069bdc91bf5f4c3
obinote <ahmad.robi.3003@gmail.com>
2022-04-04 11:58:04 +0700

    fix base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/vite-portofolio/',
+  // base: '/vite-portofolio/',
   resolve: {
     alias: {
       '@css': path.resolve(__dirname, './src/assets/style')
