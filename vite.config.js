76aafe93fe5dd41d4cf93ae5b414ac3fad300378
obinote <ahmad.robi.3003@gmail.com>
2022-04-04 11:46:08 +0700

    config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-const path = require("path");
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+const path = require('path');
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vite-portofolio/',
   resolve: {
     alias: {
-      "@css": path.resolve(__dirname, "./src/assets/style"),
-    },
+      '@css': path.resolve(__dirname, './src/assets/style')
+    }
   },
-  plugins: [react()],
+  plugins: [react()]
 });
